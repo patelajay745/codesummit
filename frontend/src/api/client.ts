5e83ebf1b965697e6834c6b3cad0c8ea35fd8541
Ajay Patel <patel.ajay745@gmail.com>
2025-05-21 11:42:11 -0400

    fixed backend url for frontend

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,9 +1,10 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: import.meta.env.NODE_ENV === "production" ? `${import.meta.env.VITE_BACKEND_BASE_URL}/api.v1` : "http://localhost:8081/api/v1",
+    baseURL: import.meta.env.NODE_ENV === "production" ? `${import.meta.env.VITE_BACKEND_BASE_URL}/api/v1` : "http://localhost:8081/api/v1",
     headers: {
         'Content-Type': "application/json"
     },
+    withCredentials: true
 
 })
