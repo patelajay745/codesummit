773e6f7a6261d1960f429af8aed6fad5f9d4dbf6
Ajay Patel <patel.ajay745@gmail.com>
2025-05-21 12:01:18 -0400

    fixed backend url for frontend

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,9 +1,7 @@
 import axios from "axios"
 
-
-
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_NODE_ENV === "production" ? `${import.meta.env.VITE_BACKEND_BASE_URL}/api/v1` : "http://localhost:8081/api/v1",
+    baseURL: import.meta.env.PROD ? `${import.meta.env.VITE_BACKEND_BASE_URL}/api/v1` : "http://localhost:8081/api/v1",
     headers: {
         'Content-Type': "application/json"
     },
--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -60,7 +60,7 @@ export const useAuthStore = create<AuthStore>((set) => ({
 
     signIn: async (data: loginFormDataTypes) => {
 
-        console.log("node_env", import.meta.env.VITE_NODE_ENV)
+        console.log("node_env", import.meta.env.PROD)
         set({ isLoggingIn: true })
         try {
             const res = await api.post("/auth/login", data)
